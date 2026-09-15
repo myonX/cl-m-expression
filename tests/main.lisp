2735e36f40143de6589f0cd6ebd29d55a18aeb49
myonX <ri.myonsan.x@gmail.com>
2026-09-15 16:39:52 +0900

    test

--- tests/main.lisp
+++ tests/main.lisp
@@ -1,6 +1,6 @@
 (defpackage cl-m-expression/tests/main
   (:use :cl
-        :clm-expression
+        :cl-m-expression
         :rove))
 (in-package :cl-m-expression/tests/main)
 
@@ -16,42 +16,19 @@ f[x;y] = x+y
 λ[[a;b;c];a+b+c]
 |#
 
-
-(deftest tokenizer-test
-         (testing "function call"
-                  (ok (equal '("f" "[" "a" ";" "b" ";" "c" "]")
-                             (cl-m-expression::tokenizer "f[a;b;c]"))))
-         
-         (testing "function call"
-                  (ok (equal '("f" "[" "x" ";" "y" "]" "=" "x+y")
-                             (cl-m-expression::tokenizer "f[x;y] = x+y"))))
-         
-         (testing "function call"
-                  (ok (equal '("[" "p_1" "->" "e_1" ";" "p_2" "->" "e_2" ";" "p_n" "->" "e_n" "]")
-                             (cl-m-expression::tokenizer "[p_1 -> e_1;p_2 -> e_2;p_n -> e_n]"))))
-         
-         (testing "function call"
-                  (ok (equal '("lambda" "[" "[" "a" ";" "b" ";" "c" "]" ";" "a+b+c" "]")
-                             (cl-m-expression::tokenizer "lambda[[a;b;c];a+b+c]"))))
-         
-         (testing "function call"
-                  (ok (equal '("fact" "[" "n" "]" "=" "[" "eq" "[" "n" ";" "1" "]" "->" "1" ";" "T" "->" "*" "[" "n" ";" "fact" "[" "-" "[" "n" ";" "1"
- "]" "]" "]" "]")
-                             (cl-m-expression::tokenizer "fact[n] = [eq[n;1] -> 1;T -> *[n;fact[-[n;1]]]]")))))
-
 (deftest parsar-test
          (testing "function call"
-                  (ok (equal (cl-m-expression::m-expression-string->s-expression "f[a;b;c]")
+                  (ok (equal (esrap:parse 'expression  "f[a;b;c]")
                              '(f a b c))))
          (testing "defun"
-                  (ok (equal (cl-m-expression::m-expression-string->s-expression "f[x;y] = g[a;b;c]")
+                  (ok (equal (esrap:parse 'expression  "f[x;y] = g[a;b;c]")
                              '(defun f (x y) (g a b c)))))
          (testing "lambda"
-                  (ok (equal (cl-m-expression::m-expression-string->s-expression "lambda[[a;b;c];+[a;b;c]]")
+                  (ok (equal (esrap:parse 'expression  "lambda[[a;b;c];+[a;b;c]]")
                              '(lambda (a b c) (+ a b c)))))
          (testing "cond"
-                  (ok (equal (cl-m-expression::m-expression-string->s-expression "[p_1 -> e_1;p_2 -> e_2;p_n -> e_n]")
+                  (ok (equal (esrap:parse 'expression  "[p_1 -> e_1;p_2 -> e_2;p_n -> e_n]")
                              '(cond (p_1 e_1) (p_2 e_2) (p_n e_n)))))
          (testing "whole"
-                  (ok (equal (cl-m-expression::m-expression-string->s-expression "fact[n] = [eq[n;1] -> 1;T -> *[n;fact[-[n;1]]]]")
+                  (ok (equal (esrap:parse 'expression  "fact[n] = [eq[n;1] -> 1;T -> *[n;fact[-[n;1]]]]")
                              '(DEFUN FACT (N) (COND ((EQ N 1) 1) (T (* N (FACT (- N 1))))))))))
